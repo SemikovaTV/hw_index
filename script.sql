e73ab70e9c4d29b1fd776d0bce701a58b1fb2843
SemikovaTV <129278233+SemikovaTV@users.noreply.github.com>
2023-07-19 19:40:18 +0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -11,11 +11,15 @@ SELECT DISTINCT CONCAT (c.last_name, ' ', c.first_name), SUM(p.amount) OVER (PAR
 FROM payment p, rental r, customer c, inventory i
 WHERE DATE(p.payment_date) = '2005-07-30' AND p.payment_date =r.rental_date AND r.customer_id=c.customer_id AND i.inventory_id =r.inventory_id
 
-EXPLAIN FORMAT = TRADITIONAL
-SELECT DISTINCT CONCAT (c.last_name, ' ', c.first_name) AS fio, SUM(p.amount)
-FROM payment p, rental r, customer c, inventory i
-WHERE DATE(p.payment_date) = '2005-07-30' AND p.payment_date =r.rental_date AND r.customer_id=c.customer_id AND i.inventory_id =r.inventory_id
+CREATE INDEX payment_date ON payment(payment_date)
+
+EXPLAIN ANALYZE
+SELECT CONCAT (c.last_name, ' ', c.first_name) AS fio, SUM(p.amount)
+FROM payment p
+inner join rental r on p.payment_date = r.rental_date
+inner join customer c on r.customer_id = c.customer_id
+inner join inventory i on i.inventory_id =r.inventory_id
+WHERE p.payment_date >= '2005-07-30' AND p.payment_date < DATE_ADD('2005-07-30', INTERVAL 1 DAY )
 GROUP BY fio
 
-CREATE INDEX payment_date ON payment(payment_date)
 
