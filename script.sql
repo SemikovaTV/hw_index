f30865b45aa307e6c0eb0ad683735c7956f3c067
SemikovaTV <129278233+SemikovaTV@users.noreply.github.com>
2023-07-19 19:36:27 +0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -19,8 +19,3 @@ GROUP BY fio
 
 CREATE INDEX payment_date ON payment(payment_date)
 
-SELECT DISTINCT CONCAT (c.last_name, ' ', c.first_name) AS fio, SUM(p.amount)
-FROM payment p, rental r, customer c, inventory i
-WHERE DATE(p.payment_date) = '2005-07-30' AND p.payment_date =r.rental_date AND r.customer_id=c.customer_id AND i.inventory_id =r.inventory_id
-GROUP BY fio
-
